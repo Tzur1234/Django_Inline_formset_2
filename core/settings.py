1d5c2abc17cd53bb6950cdbaba65133b8da466c8
root <root@DESKTOP-O1KC17T.localdomain>
2023-02-19 16:09:33 +0200

    change admin.py

--- core/settings.py
+++ core/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
+    'inventory',
+    'guardian',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +142,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend', # this is default
+    'guardian.backends.ObjectPermissionBackend',
+)
--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product
 
+from guardian.admin import GuardedModelAdmin
+
+@admin.register(Product)
+class ProductAdmin(GuardedModelAdmin):
+    # list_display = ('names',)
     
+    def has_module_permission(self, request):
+        if super().has_module_permission(request):
+            return True
+        else:
+            return True
+
+    def has_view_permission(self, request, obj=None):
+        pass
+
+
+
+
+
+
+
+
+
+
+
